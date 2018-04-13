b0cc9b72d3398b873e01805a4fcb7b73517d3dcf
etbrem <etbrem2@gmail.com>
2018-04-13 16:25:38 +0300

    Renamed SearchEngineBaseClass

--- search_engines/search_engine_base.py
+++ search_engines/search_engine_base.py
@@ -4,5 +4,5 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from auto_cascade import *
 
 
-class SearchEngineBase(MyAbstractClass):
+class SearchEngineBaseClass(MyAbstractClass):
     abstract_methods = ('search', 'parse_magnet_results', 'parse_torrent_results')
